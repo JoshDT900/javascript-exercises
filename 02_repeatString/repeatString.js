3a8d7d90823f9f56bcb49242411df0a1359215b1
JoshDT900 <DJTroutman900@gmail.com>
2022-06-06 19:53:06 -0400

    Complete repeatString assignment

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  let repeatThis = string;
+  let timesRepeated = num;
 
+  let result = "";
+
+  if (num < 0) {
+    return result = "ERROR";
+  }
+
+  for (let i = 0; i < timesRepeated; i++) {
+    result += `${repeatThis}`;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
