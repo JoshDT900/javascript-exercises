f074d46d74a86b79c418c31060183ce10e6121b9
JoshDT900 <DJTroutman900@gmail.com>
2022-06-07 09:33:37 -0400

    Complete leapYears unit test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,15 @@
 const leapYears = function() {
+  let year = arguments[0];
+
+  if (year % 400 === 0) {
+    return true;
+  } else if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 
 };
 
