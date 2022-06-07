f53f7b39a35003f778bc5fb0ad1562d22d11feed
JoshDT900 <DJTroutman900@gmail.com>
2022-06-06 20:31:18 -0400

    Complete removeFromArray tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
 const removeFromArray = function() {
+  let storeArr = [];
 
+  for (let i = 0; i < arguments.length; i++) {    
+    if (Array.isArray(arguments[i])) {
+      storeArr = arguments[i];
+    }
+
+    if (storeArr.indexOf(arguments[i]) > -1) {
+      let index = storeArr.indexOf(arguments[i]);
+      storeArr.splice(index, 1);
+    }
+  }
+
+  return storeArr;
 };
 
 // Do not edit below this line
