b5bfd959824e893cd55aae531126f0ca010319c0
JoshDT900 <DJTroutman900@gmail.com>
2022-06-07 08:34:37 -0400

    Complete all sumAll tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
 const sumAll = function() {
+  let argArr = [...arguments];
+  let total = 0;
 
+  argArr.sort((a,b) => a - b);
+  
+  let argOne = argArr[0];
+  let argTwo = argArr[1];
+
+  if (typeof argOne != "number" || typeof argTwo != "number") {
+    return "ERROR";
+  }
+
+  for (let i = argArr[0]; i <= argArr[1]; i++) {
+    if (argArr[i] < 0) {      
+      return "ERROR";
+    } else {
+      total += i;
+    }    
+  }
+
+  return total;
 };
+
+sumAll(1, 4)
 
 // Do not edit below this line
 module.exports = sumAll;
