8b13eeb35a60823553c4233f5f8e5c546cc85895
JoshDT900 <DJTroutman900@gmail.com>
2022-06-07 09:54:10 -0400

    Complete unit tests for tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,25 @@
 const ftoc = function() {
+  let fahren = arguments[0];
 
+  let celcius = (fahren - 32) * 5/9;
+
+  if (Number.isInteger(celcius) === false ) {
+    return parseFloat(celcius.toFixed(1)) 
+  } else {
+    return celcius
+  }
 };
 
 const ctof = function() {
+  let celcius = arguments[0]
+
+  let fahren = celcius * 9/5 + 32;
+
+  if (Number.isInteger(fahren) === false ) {
+    return parseFloat(fahren.toFixed(1)) 
+  } else {
+    return fahren
+  }
 
 };
 
