0abe1dd83df65521a9ced435cac8854089479253
JoshDT900 <DJTroutman900@gmail.com>
2022-06-11 15:18:53 -0400

    Finish fibonacci tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(val) {
+  let fibVal = val;
+  let conditionVal = val;
 
+  if (fibVal < 0) {
+    return "OOPS";
+  } else if (typeof fibVal === 'string'){
+    parseInt(fibVal)
+  }
+
+  let firstVal = 1;
+  let secondVal = 0;
+  let temp = 0;
+
+  let fibArr = [];
+
+  while (conditionVal >= 0) {
+    temp = firstVal;
+    firstVal = firstVal + secondVal;
+    secondVal = temp;
+    fibArr.push(secondVal);
+    conditionVal--;
+  }
+
+  return fibArr[fibVal - 1];
 };
 
 // Do not edit below this line
