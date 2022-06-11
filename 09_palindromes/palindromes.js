ded647f94cf7e3344df5b8eb326dfff88ed61428
JoshDT900 <DJTroutman900@gmail.com>
2022-06-11 13:38:44 -0400

    Finish palindromes unit tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let splitUp = string.toLowerCase().split("").reverse().join("").replace(/[^a-z0-9]/gi, '')
 
+  return splitUp === string.replace(/[^a-z0-9]/gi, '').toLowerCase();
 };
 
 // Do not edit below this line
