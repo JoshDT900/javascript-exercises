5bc11e3184862aca4e2a2e6cb5f7fc76cd007e7b
JoshDT900 <DJTroutman900@gmail.com>
2022-06-11 19:07:24 -0400

    Finish unit tests

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
 const getTheTitles = function() {
+  let mainArr = arguments[0];
+  let bookArr = [];
 
+  for (let i of mainArr) {    
+    bookArr.push(i.title)
+  }
+
+  return bookArr;
 };
 
 // Do not edit below this line
